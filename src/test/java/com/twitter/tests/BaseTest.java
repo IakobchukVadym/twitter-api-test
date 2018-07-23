d594b02f9ad95493263406f6ef516c7a74fb9017
Vadym Iakobchuk <iakobchuk.v@gmail.com>
2018-07-24 00:30:41 +0200

    code refactoring

--- src/test/java/com/twitter/tests/BaseTest.java
+++ src/test/java/com/twitter/tests/BaseTest.java
@@ -13,12 +13,12 @@ public class BaseTest {
     protected String consumerKey = "HQqWDhlNh48NJXGBVkLNsZILJ";
     protected String consumerSecret = "BdhxsE5CGFCyS9XVF2FiBK6ZC6Wnz5ytJfyMvs1R5EpLERyAMc";
     protected String accessToken = "1020943670764363778-yjVWe2Q3twis8snUb32mVSnU318iTI";
-    protected String accessSecret= "PzxjtUyyFz9BctYZTnCEO5sFqI58CxQ96XKccwP2LxFkH";
+    protected String accessSecret = "PzxjtUyyFz9BctYZTnCEO5sFqI58CxQ96XKccwP2LxFkH";
 
     protected String baseURI = "https://api.twitter.com/1.1/";
 
     protected RequestSpecification oauth1 = new RequestSpecBuilder().setBaseUri(baseURI).
-            setAuth(oauth(consumerKey,consumerSecret,accessToken,accessSecret)).build();
+            setAuth(oauth(consumerKey, consumerSecret, accessToken, accessSecret)).build();
 
 
     protected ResponseSpecification timeSpec = new ResponseSpecBuilder().
