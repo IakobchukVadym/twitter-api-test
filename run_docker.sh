591aaf89125d6724cb58b93217bb5ff28a6993a0
Vadym Iakobchuk <iakobchuk.v@gmail.com>
2018-07-26 00:08:33 +0200

    adding scripts

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-docker build -t apitest .
-docker run --rm -it -v $(pwd)/target/surefire-reports:/usr/src/apitesting/target/surefire-reports api-test-docker:latest mvn clean test
-
-docker run -it --rm --name my-maven-project -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven api-test-docker:latest mvn clean test
+docker run -it --rm --name my-maven-project -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven api-test-docker:latest mvn clean -Dmaven.clean.failOnError=false test
