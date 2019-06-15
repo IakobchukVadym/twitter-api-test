2665cb7ff660856072990b92a28b4101f90faa02
Vadym Iakobchuk <iakobchuk.v@gmail.com>
2019-06-15 20:12:13 +0200

    Add properties and decryption for api keys
    Ignore log for unstable test

--- src/main/java/com/twitter/utils/CommonMethods.java
+++ src/main/java/com/twitter/utils/CommonMethods.java
@@ -1,13 +1,16 @@
-package com.twitter.common;
+package com.twitter.utils;
 
 import java.security.SecureRandom;
 
-public class CommonMethods {
+public final class CommonMethods {
 
     private static final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     private static SecureRandom rnd = new SecureRandom();
 
-    public static String getRandomString(int len){
+   private CommonMethods() {
+   }
+
+   public static String getRandomString(int len) {
         StringBuilder sb = new StringBuilder( len );
         for( int i = 0; i < len; i++ )
             sb.append( AB.charAt( rnd.nextInt(AB.length()) ) );
--- src/test/java/com/twitter/tests/TestTwitCreationAndDeletion.java
+++ src/test/java/com/twitter/tests/TestTwitCreationAndDeletion.java
@@ -1,6 +1,7 @@
 package com.twitter.tests;
 
-import com.twitter.common.BaseTest;
+import com.twitter.utils.BaseTest;
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
@@ -12,53 +13,61 @@ import static org.hamcrest.Matchers.hasItem;
 
 public class TestTwitCreationAndDeletion extends BaseTest {
 
-    private String twitId;
-    private String textToPublish = "This is posted as part of Automation Test executed on " + new Date().toString();
+  private String twitId;
+  private String textToPublish =
+     "This is posted as part of Automation Test executed on " + new Date().toString();
 
-    //Create new twit
-    @Test
-    public void verifyTwitCreation() {
-        Response response =
-                given().
-                        spec(oauth1).
-                        queryParam("status", textToPublish).
-                        when().
-                        post("statuses/update.json").
-                        then().
-                        log().ifValidationFails().
-                        statusCode(200).
-                        body("text", equalTo(textToPublish)).
-                        extract().response();
-        twitId = response.path("id_str");
-    }
+  // Create new twit
+  @Test
+  public void verifyTwitCreation() {
+    Response response =
+       given()
+          .spec(oauth1)
+          .queryParam("status", textToPublish)
+          .filter(new AllureRestAssured())
+          .when()
+          .post("statuses/update.json")
+          .then()
+          .log()
+          .ifValidationFails()
+          .statusCode(200)
+          .body("text", equalTo(textToPublish))
+          .extract()
+          .response();
+    twitId = response.path("id_str");
+  }
 
-    //Check that duplication prevention works
-    @Test(dependsOnMethods = "verifyTwitCreation")
-    public void verifyDuplicatePrevention() {
-        given().
-                spec(oauth1).
-                queryParam("status", textToPublish).
-                when().
-                post("/statuses/update.json").
-                then().
-                log().ifValidationFails().
-                statusCode(403).
-                root("errors").
-                body("code", hasItem(187)).
-                body("message", hasItem("Status is a duplicate."));
-    }
+  // Check that duplication prevention works
+  @Test(dependsOnMethods = "verifyTwitCreation")
+  public void verifyDuplicatePrevention() {
+    given()
+       .spec(oauth1)
+       .queryParam("status", textToPublish)
+       .filter(new AllureRestAssured())
+       .when()
+       .post("/statuses/update.json")
+       .then()
+       .log()
+       .ifValidationFails()
+       .statusCode(403)
+       .root("errors")
+       .body("code", hasItem(187))
+       .body("message", hasItem("Status is a duplicate."));
+  }
 
-    //Delete last created twit
-    @Test(dependsOnMethods = "verifyDuplicatePrevention")
-    public void verifyTwitDeletion() {
-        given().
-                spec(oauth1).
-                when().
-                post("/statuses/destroy/" + twitId + ".json").
-                then().
-                log().ifValidationFails().
-                statusCode(200).
-                body("text", equalTo(textToPublish)).
-                body("id_str", equalTo(twitId));
-    }
+  // Delete last created twit
+  @Test(dependsOnMethods = "verifyDuplicatePrevention")
+  public void verifyTwitDeletion() {
+    given()
+       .spec(oauth1)
+       .when()
+       .filter(new AllureRestAssured())
+       .post("/statuses/destroy/" + twitId + ".json")
+       .then()
+       .log()
+       .ifValidationFails()
+       .statusCode(200)
+       .body("text", equalTo(textToPublish))
+       .body("id_str", equalTo(twitId));
+  }
 }
