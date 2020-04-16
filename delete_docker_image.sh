f7400c7c952f6e855692c0d32067e8a354be5121
Vadym Iakobchuk <iakobchuk.v@gmail.com>
2020-04-17 00:01:19 +0200

    Add docker compose

--- delete_docker_image.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ "$(docker images -a | grep "api-test-docker")" ]]; then
-    echo "Deleting image"
-    docker rmi "api-test-docker"
-else
-    echo "Image api-test-docker does not exists"
-fi
--- run_docker.sh
+++ run_docker.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
-if [ ! "$(docker images -a | grep api-test-docker)" ]; then
-    echo "Building image"
-    docker build --tag api-test-docker .
-fi
+rm -r allure-results/*
 
-docker run -it --rm --name my-maven-project -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven api-test-docker:latest mvn clean -Denv=prod -Dmaven.clean.failOnError=false test
+checkTests() {
+    while [ "$(docker ps | grep api-mvn-test)" ]
+        do
+            echo "Tests are running..."
+            sleep 7
+        done
+     echo Allure report is available on http://localhost:4040
+}
+
+if [ ! "$(docker ps | grep api-allure-test)" ]
+then
+    docker-compose up -d
+    checkTests
+else
+    docker start mvn-test
+    checkTests
+fi
